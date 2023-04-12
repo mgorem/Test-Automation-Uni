f977ef137828038b7d14ce800ea75f1f622f7c8b
/omg <oremgitonga22@gmail.com>
2023-04-12 16:25:13 +0300

    overloading methods

---
+++ src/ch6d/MonthConverter.java
@@ -0,0 +1,9 @@
+package ch6d;
+
+public class MonthConverter {
+
+    public static void main(String args[]){
+        System.out.println(Month.getMonth(2));
+        System.out.println(Month.getMonth("January"));
+    }
+}
