98bd2da406051bcd68d486c58f299b385b1736cb
/omg <oremgitonga22@gmail.com>
2023-04-13 14:29:33 +0300

    useful array tricks, data types, strings, inheritance

---
+++ src/ch9a/Employee.java
@@ -0,0 +1,23 @@
+package ch9a;
+
+public class Employee extends Person {
+    public String getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String EmployeeId;
+    private String title;
+
+}
---
+++ src/ch9a/InheritanceTester.java
@@ -0,0 +1,8 @@
+package ch9a;
+
+public class InheritanceTester {
+    public static void main(String[] args){
+        Person person = new Person();
+        Employee employee = new Employee();
+    }
+}
